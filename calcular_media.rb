dae1c2ab97d85ea3561d91fffd5bd14f758108c7
Leiry Dharya <lleiry1989@gmail.com>
2020-02-15 15:57:36 -0300

    calculo de media

---
+++ calcular_media.rb
@@ -0,0 +1,15 @@
+puts "digite a nota 1: "
+    nota1 = gets
+
+puts "digite a nota 2: "
+    nota2 = gets
+
+puts "digite nota 3: "
+    nota3 = gets
+
+puts "digite nota 4: "
+    nota4 = gets
+
+media = nota1.to_f + nota2.to_f + nota3.to_f + nota4.to_f / 4
+
+puts "resultado: #{media}"
