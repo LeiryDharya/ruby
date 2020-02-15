fac283830c796c85a39ced64e7dafce6dc1e4108
Leiry Dharya <lleiry1989@gmail.com>
2020-02-15 17:37:10 -0300

    cadastro de cliente

---
+++ agenda.rb
@@ -0,0 +1,22 @@
+puts "Informe nome: "
+    cliente = gets
+
+puts "Informe seu e-mail: "
+    email = gets
+
+puts "Informe telefone: "
+    telefone = gets
+
+puts ""
+
+puts "=========================================="
+puts "Informações cadastradas com sucesso!"
+puts "=========================================="
+
+puts "Cliente: #{cliente}"
+
+puts "E-mail: #{email}"
+
+puts "Telefone: #{telefone}"
+
+puts "------------------------------------------"
